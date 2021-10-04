b0dfb03563bcd93c9609bae85af2be540118b28c
secxndary <valdaitsevv@mail.ru>
2021-10-04 12:52:13 +0300

    Lab5 ready

--- OOP_Lab5/OOP_Lab5/Classes.cs
+++ OOP_Lab5/OOP_Lab5/Classes.cs
@@ -4,21 +4,17 @@ using System.Text;
 
 namespace OOP_Lab5
 {
-    interface IName
+    public interface IName
     {
         string Name { get { return Name; } set { this.Name = value; } }
-        void showInfo();
     }
-    public abstract class IntelligentBeing: IName
+    public abstract class IntelligentBeing : IName
     {
         public string Name { get; set; }
-        public virtual void showInfo()
-        {
-            Console.WriteLine("Имя: " + Name);
-        }
+
     }
 
-    public class Person: IntelligentBeing, IDrive
+    public class Person : IntelligentBeing, IDrive
     {
         public string LastName { get; set; }
 
@@ -28,11 +24,10 @@ namespace OOP_Lab5
             this.LastName = lastName;
         }
 
-        public override void showInfo()
+
+        public override string ToString()
         {
-            Console.WriteLine("Имя: " + Name);
-            Console.WriteLine("Фамилия: " + LastName);
-            Console.WriteLine(new String('-', 30));
+            return "Тип объекта: " + GetType().Name + "\nИмя: " + Name + "\nФамилия: " + LastName + "\n" + new String('-', 50);
         }
 
         public void Drive(ICarManagement carManagement)
@@ -50,19 +45,16 @@ namespace OOP_Lab5
             this.Nickname = nickName;
             this.Power = power;
         }
-        public override void showInfo()
-        {
-            Console.WriteLine("Имя: " + Name);
-            Console.WriteLine("Прозвище: " + Nickname);
-            Console.WriteLine("Мощность двигателя: " + Power);
-            Console.WriteLine(new String('-', 30));
 
+        public override string ToString()
+        {
+            return "Тип объекта: " + GetType().Name + "\nИмя: " + Name + "\nПрозвище: " + Nickname + "\nМощность двигателя: " + Power + "\n" + new String('-', 50);
         }
         public void StartEngine(Engine engine)
         {
             if (engine.TryPowering())
                 Console.WriteLine("Трансформер завёлся)");
-            else Console.WriteLine("Он слишком стар....");
+            else Console.WriteLine("Трансформер слишком стар....");
         }
 
     }
@@ -78,18 +70,58 @@ namespace OOP_Lab5
         public int Power { get; set; }
     }
 
-    public abstract class Vehicle: ICarManagement
+    public abstract class Vehicle : ICarManagement, IName
     {
         public int Tires { get; set; }
         public int Power { get; set; }
 
-        public void StartEngine(Engine engine) 
+        public virtual void StartEngine(Engine engine)
         {
             if (engine.TryPowering())
                 Console.WriteLine("Получилось! Двигатель завёлся!");
             else Console.WriteLine("Завестись не получилось...");
         }
+        public override bool Equals(object obj)
+        {
+            if (obj is Vehicle && obj != null)
+            {
+                return this.GetType() == obj.GetType();
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return 10654;
+        }
+
+
     }
+
+
+    public class Printer
+    {
+        public string IAmPrinting(Vehicle obj)
+        {
+            if (obj is Car)
+            {
+                Car c = (Car)obj;
+                return "Тип объекта: " + c.GetType().Name + "\nНазвание машины: " + c.Name + "\nМощность двигателя: " + c.Power + "\nКол-во колёс: " + c.Tires + "\n" + new String('-', 50);
+            }
+            if (obj is Tank)
+            {
+                Tank t = (Tank)obj;
+                return "Тип объекта: " + t.GetType().Name + "\nНазвание танка: " + t.Name + "\nМощность двигателя: " + t.Power + "\nКол-во колёс: " + t.Tires + "\n" + new String('-', 50);
+            }
+            if (obj is Bike)
+            {
+                Bike b = (Bike)obj;
+                return "Тип объекта: " + b.GetType().Name + "\nНазвание байка: " + b.Name + "\nМощность двигателя: " + b.Power + "\nКол-во колёс: " + b.Tires + "\n" + new String('-', 50);
+            }
+            return "qq";
+        }
+    }
+
 
     public class Car: Vehicle, IName
     {
@@ -102,12 +134,48 @@ namespace OOP_Lab5
             this.Power = power;
         }
 
-        public  void showInfo()
+        public override void StartEngine(Engine engine)
         {
-            Console.WriteLine("Название машины: " + Name);
-            Console.WriteLine("Мощность двигателя: " + Power);
-            Console.WriteLine("Кол-во колёс: " + Tires);
-            Console.WriteLine(new String('-', 30));
+            if (engine.TryPowering())
+                Console.WriteLine("Машина стартанула! Поехали!");
+            else Console.WriteLine("Не заводится... Ты точно сцепление выжал?");
+        }
+
+        public override string ToString()
+        {
+            return "Тип объекта: " + GetType().Name + "\nНазвание машины: " + Name + "\nМощность двигателя: " + Power + "\nКол-во колёс: " + Tires + "\n" + new String('-', 50);
+        }
+    }
+
+    public class Tank : Vehicle, IName
+    {
+        public string Name { get; set; }
+
+        public Tank(string name, int power)
+        {
+            this.Tires = 16;
+            this.Name = name;
+            this.Power = power;
+        }
+        public override void StartEngine(Engine engine)
+        {
+            if (engine.TryPowering())
+                Console.WriteLine("ТАНК ЗАВЕЛСЯ ВСЕМ ПИЗДА");
+            else Console.WriteLine("С танком проблемы. Может попробовать завести трансформера)?");
+        }
+        public override string ToString()
+        {
+            return "Тип объекта: " + GetType().Name + "\nНазвание танка: " + Name + "\nМощность двигателя: " + Power + "\nКол-во колёс: " + Tires + "\n" + new String('-', 50);
+        }
+    }
+
+    public class Bike: Car, IName
+    {
+        public string Company { get; set; }
+        public Bike(string name, int power, string company): base(name, power)
+        {
+            this.Tires = 2;
+            this.Company = company;
         }
     }
 
--- OOP_Lab5/OOP_Lab5/Program.cs
+++ OOP_Lab5/OOP_Lab5/Program.cs
@@ -7,15 +7,42 @@ namespace OOP_Lab5
         static void Main(string[] args)
         {
             Person p = new Person("Валера", "Холера");
-            p.showInfo();
-            Transformer trans = new Transformer("Оптимус", "Прайм", 8777);
+            Transformer t = new Transformer("Оптимус", "Прайм", 8777);
             Car c = new Car("Supra", 780);
+            Car cClone = new Car("Supra", 780);
             Car c1 = new Car("Nissan 240-SX", 630);
-            ICarManagement[] vehicle = { trans, c, c1 };
+            Tank t1 = new Tank("Panzerkampfwagen IV", 1200);
+
+            Console.WriteLine(p.ToString());
+            ICarManagement[] vehicle = { t, c, c1, t1 };
             foreach (ICarManagement item in vehicle)
             {
                 p.Drive(item);
+                Console.WriteLine(item.ToString());
             }
+
+            object bIs = new Bike("Аист", 30, "Вепрь");
+            if (bIs is Bike)
+            {
+                Console.WriteLine("\nОбъект преобразуем к типу Bike.");
+            }
+            else Console.WriteLine("Преобразование прошло не успешно.");
+            Bike bAs = c as Bike;
+            if (bAs == null) Console.WriteLine("Преобразование прошло не успешно.");
+            else Console.WriteLine(bAs.Company);
+
+            ICloneable user1 = new User();
+            user1.DoClone();
+            BaseClone user2 = new User();
+            user2.DoClone(false);
+
+            if (c.Equals(cClone))
+                Console.WriteLine("\nМашины идентичны\n");
+            else Console.WriteLine("\nМашины не идентичны\n");
+
+            Printer print = new Printer();
+            Console.WriteLine(print.IAmPrinting(c)); 
+            Console.WriteLine(print.IAmPrinting(t1)); 
         }
     }
 }
