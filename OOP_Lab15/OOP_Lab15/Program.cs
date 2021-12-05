78270517fdff5c1fb3a1300d38d13dc329fbbeb9
secxndary <valdaitsevv@mail.ru>
2021-12-05 13:12:18 +0300

    Lab 15 v.0.1

---
+++ OOP_Lab15/OOP_Lab15/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+
+namespace OOP_Lab15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var allProcesses = Process.GetProcesses();
+            foreach (var proc in allProcesses)
+            {
+                Console.WriteLine("ID: " + proc.Id);
+                Console.WriteLine("Name: " + proc.ProcessName);
+                Console.WriteLine();
+            }
+        }
+    }
+}
